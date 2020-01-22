e4d1a719cbe0ddda1d4efa8a04a126aec1b07f04
BradT1759 <BradT1759@users.noreply.github.com>
2020-01-22 17:30:26 -0500

    Add files via upload
    Updated with a method so the user's selection will be taken by the computer for this Rock Paper Scissors game. Hopefully it works, and the user's choice is displayed along with the computer's.

--- lab1.js
+++ lab1.js
@@ -1,9 +1,18 @@
 
 prompt ("This is a game of Rock, Paper, Scissors. Which will you choose for this round?");
+const getUserSelection = userInput => {
+ userInput = userInput.toLowerCase(); 
+  if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
+    return userInput;
+  } else {
+    console.log('Error!');
+  }
+}
 var computerChoice = Math.random();
 {
 if (0 >= 0.33) { console.log("Rock"); } 
 else if (0.34 <= 0.66) { console.log("Paper"); } 
 else if (0.67 <= 1) { console.log("Scissors"); }
 }
+console.log(userSelection);
 console.log(computerChoice);
