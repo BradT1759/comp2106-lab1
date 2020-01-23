374c4590eb00c57d756f658ead30b6192caa4554
BradT1759 <BradT1759@users.noreply.github.com>
2020-01-23 18:07:21 -0500

    Add files via upload

--- lab1.js
+++ lab1.js
@@ -1,9 +1,9 @@
-prompt ("Which will you choose? Rock, Paper or Scissors?");
+var userSelection = window.prompt ("Which will you choose? Rock, Paper or Scissors?");
 var computerChoice = Math.random();
 {
 if (0 >= 0.33) { console.log("Rock"); } 
 else if (0.34 <= 0.66) { console.log("Paper"); } 
 else if (0.67 <= 1) { console.log("Scissors"); }
 }
-
+console.log(userSelection);
 console.log(computerChoice);
